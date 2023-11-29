2df93d8df67be78e816bd638ae2e1f2485711fce
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2023-11-29 15:40:11 +0530

    Update 8-2-serverf.sh

--- 8-2-serverf.sh
+++ 8-2-serverf.sh
@@ -6,3 +6,4 @@ radosgw-admin zonegroup default --rgw-zonegroup=classroom
 radosgw-admin zone create --rgw-zonegroup=classroom --rgw-zone=us-east-2 --endpoints=http://serverf:80 --access-key=replication --secret-key=secret --default
 radosgw-admin period update --commit --rgw-zone=us-east-2
 ceph config set client.rgw rgw_zone us-east-2
+ceph orch apply rgw cl260-2 --realm=cl260 --zone=us-east-2 --placement="1 serverf.lab.example.com"
