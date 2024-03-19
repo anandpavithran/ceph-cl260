27a819a2a6a81cd12f31e0707279a7e5b9a0b845
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2024-03-19 17:36:19 +0530

    Update sec6-2-clienta.cmd

--- sec6-2-clienta.cmd
+++ sec6-2-clienta.cmd
@@ -2,7 +2,7 @@ cephadm shell -- ceph health
 cephadm shell -- ceph osd pool create test_pool 32 32
 cephadm shell -- rbd pool init test_pool
 cephadm shell -- ceph df
-cephadm shell -- ceph auth get-or-create client.test_pool.clientb mon 'profile rbd' osd 'profi le rbd' | sudo tee /etc/ceph/ceph.client.test_pool.clientb.keyring
+cephadm shell -- ceph auth get-or-create client.test_pool.clientb mon 'profile rbd' osd 'profile rbd' | sudo tee /etc/ceph/ceph.client.test_pool.clientb.keyring
 cat /etc/ceph/ceph.client.test_pool.clientb.keyring
 cephadm shell -- ceph auth get client.test_pool.clientb
 ssh root@serverb "yum install -y ceph-common"
