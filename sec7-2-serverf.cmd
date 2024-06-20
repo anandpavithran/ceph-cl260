e9935c39e6d3ab41e88094be679ba6a67c460635
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2024-06-20 15:26:18 +0530

    Update sec7-2-serverf.cmd

--- sec7-2-serverf.cmd
+++ sec7-2-serverf.cmd
@@ -2,9 +2,10 @@ cephadm shell -- ceph status
 cephadm shell -- ceph osd pool create rbd 32 32
 cephadm shell -- ceph osd pool application enable rbd rbd
 cephadm shell -- rbd pool init -p rbd
+go to clienta
 In another terminal of serverf cephadm shell --mount /root/bootstrap_token_prod
-cephadm shell -- ceph orch apply rbd-mirror --placement=serverf.lab.example.com
-cephadm shell -- rbd mirror pool peer bootstrap import --site-name bup --direction rx-only rbd /mnt/bootstrap_token_prod
+In another terminal of serverf ceph orch apply rbd-mirror --placement=serverf.lab.example.com
+In another terminal of serverf rbd mirror pool peer bootstrap import --site-name bup --direction rx-only rbd /mnt/bootstrap_token_prod
 cephadm shell -- rbd -p rbd ls
 Go to clienta
 cephadm shell -- rbd mirror pool info rbd 
