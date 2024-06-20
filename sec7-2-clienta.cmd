0250bbf1c0699d1a84c45a60efb9bbf4694e9ae0
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2024-06-20 13:49:02 +0530

    Create sec7-2-clienta.cmd

---
+++ sec7-2-clienta.cmd
@@ -0,0 +1,17 @@
+cephadm shell -- ceph status
+cephadm shell -- ceph osd pool create rbd 32 32
+cephadm shell -- ceph osd pool application enable rbd rbd
+cephadm shell -- rbd pool init -p rbd
+cephadm shell -- rbd create image1 --size 1024 --pool rbd --image-feature=exclusive-lock,journaling
+cephadm shell -- rbd -p rbd ls
+cephadm shell -- rbd --image image1 info
+cephadm shell -- rbd mirror pool enable rbd pool
+cephadm shell -- rbd --image image1 info
+mkdir /root/mirror
+In another terminal of clienta cephadm shell --mount /root/mirror/
+In another terminal of clienta rbd mirror pool peer bootstrap create --site-name prod rbd > /mnt/bootstrap_token_prod
+rsync -avP /root/mirror/bootstrap_token_prod serverf:/root/bootstrap_token_prod
+cephadm shell -- rbd mirror pool info rbd
+cephadm shell -- rbd rm image1 -p rbd
+cephadm shell -- rbd -p rbd ls
+use exit
