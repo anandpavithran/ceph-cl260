565de22dfc38ac4effe0c693a90f7d5abfbf8c54
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2024-06-20 15:29:17 +0530

    Delete b.sh

--- b.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ceph osd crush move rack1 root=default-cl260
-ceph osd crush move hostc rack=rack1
-ceph osd crush move rack2 root=default-cl260
-ceph osd crush move hostd rack=rack2
-ceph osd crush move rack3 root=default-cl260
-ceph osd crush move hoste rack=rack3
